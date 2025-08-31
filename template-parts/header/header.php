5e06b6cd26dbb37fc9be4dfac75c5fab41e8f054
Md. Kazim <makalamin012@gmail.com>
2025-08-31 18:51:55 +0600

    refreshing css

--- template-parts/header/header.php
+++ template-parts/header/header.php
@@ -32,6 +32,13 @@ $vat_checked = (isset($_COOKIE['exclude_tax']) && $_COOKIE['exclude_tax'] == 'ye
             </div>
         </div>
 
+        <?php if (!wp_is_mobile()) { ?>
+            <form id="wpboilerplate-product-search" class="wpboilerplate-search-box hide-on-mobile">
+                <input type="text" name="product_search" id="product_search" placeholder="Search..." onfocus="WPBoilerplateLBInitSearch()" onkeyup="WPBoilerplateLBInitSearch()">
+                <span><?php echo $search_icon; ?></span>
+            </form>
+        <?php } ?>
+
         <div id="wpboilerplate_main_menu">
             <?php
             wp_nav_menu(array(
@@ -41,100 +48,81 @@ $vat_checked = (isset($_COOKIE['exclude_tax']) && $_COOKIE['exclude_tax'] == 'ye
                 'container_class' => 'collapse navbar-collapse',
                 'container_id' => 'wpboilerplate_main_menu',
             ));
-            ?>
 
-            <?php if (!wp_is_mobile()) { ?>
-                <form id="wpboilerplate-product-search" class="wpboilerplate-search-box hide-on-mobile">
-                    <input type="text" name="product_search" id="product_search" placeholder="Search..." onfocus="WPBoilerplateLBInitSearch()" onkeyup="WPBoilerplateLBInitSearch()">
-                    <span><?php echo $search_icon; ?></span>
-                </form>
-            <?php } ?>
+            if (is_plugin_active('woocommerce/woocommerce.php')) {
 
-            <div class="wpboilerplate-header-right">
-                <?php
-                if (is_plugin_active('woocommerce/woocommerce.php')) {
-                    if (!wp_is_mobile()) {
-                ?>
-                        <label class="tax-switch" style="display: flex; align-items: center; cursor: pointer; margin-right: 24px;">
-                            Exclude VAT
-                            <input type="checkbox" id="tax_toggle"
-                                style="margin-right: 5px;" <?php echo $vat_checked; ?>>
-                            <span class="slider round"></span>
-                        </label>
-                    <?php } ?>
+                echo '<div class="wpboilerplate-woo-navs">';
 
-                    <!-- Wishlist Button -->
-                    <?php if (is_plugin_active('yith-woocommerce-wishlist/init.php')) { ?>
-                        <a href="<?php echo esc_url(YITH_WCWL()->get_wishlist_url()); ?>" class="wishlist-icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                                <path d="M16.2201 3.32846C13.9854 1.95769 12.035 2.51009 10.8633 3.39001C10.3828 3.7508 10.1426 3.93119 10.0013 3.93119C9.85997 3.93119 9.6198 3.7508 9.1393 3.39001C7.96765 2.51009 6.01721 1.95769 3.7825 3.32846C0.849693 5.12745 0.186069 11.0624 6.95091 16.0695C8.2394 17.0232 8.88363 17.5 10.0013 17.5C11.119 17.5 11.7632 17.0232 13.0517 16.0695C19.8166 11.0624 19.1529 5.12745 16.2201 3.32846Z" stroke="#6F7A93" stroke-width="1.5" stroke-linecap="round" />
-                            </svg>
-                            <span class="wishlist-count"><?php echo yith_wcwl_count_products(); ?></span>
-                        </a>
-                    <?php } ?>
-
-                    <!-- Cart Button -->
-                    <a href="<?php echo wc_get_cart_url(); ?>" class="cart-icon">
-                        <svg width="45" height="36" viewBox="0 0 45 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M44.1199 7.0763L41.1473 1.93345C40.4582 0.740311 39.1827 0.00488281 37.8096 0.00488281H31.129L40.7822 16.7192L44.1199 10.9334C44.809 9.7403 44.809 8.26945 44.1199 7.0763Z" fill="#884DF8" />
-                            <path d="M37.4464 22.4997C38.1356 21.3066 38.1356 19.8357 37.4464 18.6426L27.7984 1.93345C27.1093 0.740311 25.8339 0.00488281 24.4556 0.00488281H17.775L34.1036 28.2854L37.4413 22.4997H37.4464Z" fill="#884DF8" />
-                            <path d="M20.7514 35.9994C22.8817 35.9994 24.6086 34.2725 24.6086 32.1423C24.6086 30.0121 22.8817 28.2852 20.7514 28.2852C18.6212 28.2852 16.8943 30.0121 16.8943 32.1423C16.8943 34.2725 18.6212 35.9994 20.7514 35.9994Z" fill="#884DF8" />
-                            <path d="M34.1073 35.9994C36.2375 35.9994 37.9644 34.2725 37.9644 32.1423C37.9644 30.0121 36.2375 28.2852 34.1073 28.2852C31.977 28.2852 30.2501 30.0121 30.2501 32.1423C30.2501 34.2725 31.977 35.9994 34.1073 35.9994Z" fill="#884DF8" />
-                            <path d="M14.4463 1.92857C13.7571 0.735428 12.4817 0 11.1086 0H4.428H0L4.45885 7.71428H8.8817L20.7565 28.2805L24.0994 22.4948C24.7885 21.3017 24.7885 19.8308 24.0994 18.6377L14.4463 1.92857Z" fill="#884DF8" />
+                if (is_plugin_active('yith-woocommerce-wishlist/init.php')) { ?>
+                    <a href="<?php echo esc_url(YITH_WCWL()->get_wishlist_url()); ?>" class="wishlist-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                            <path d="M16.2201 3.32846C13.9854 1.95769 12.035 2.51009 10.8633 3.39001C10.3828 3.7508 10.1426 3.93119 10.0013 3.93119C9.85997 3.93119 9.6198 3.7508 9.1393 3.39001C7.96765 2.51009 6.01721 1.95769 3.7825 3.32846C0.849693 5.12745 0.186069 11.0624 6.95091 16.0695C8.2394 17.0232 8.88363 17.5 10.0013 17.5C11.119 17.5 11.7632 17.0232 13.0517 16.0695C19.8166 11.0624 19.1529 5.12745 16.2201 3.32846Z" stroke="#6F7A93" stroke-width="1.5" stroke-linecap="round" />
                         </svg>
-
-                        <span class="cart-count"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
+                        <span class="wishlist-count"><?php echo yith_wcwl_count_products(); ?></span>
                     </a>
+                <?php } ?>
 
-                    <?php
-                    // Check if user is logged in
-                    if (is_user_logged_in()) {
-                        $user_id            = get_current_user_id();
+                <!-- Cart Button -->
+                <a href="<?php echo wc_get_cart_url(); ?>" class="cart-icon">
+                    <svg width="45" height="36" viewBox="0 0 45 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M44.1199 7.0763L41.1473 1.93345C40.4582 0.740311 39.1827 0.00488281 37.8096 0.00488281H31.129L40.7822 16.7192L44.1199 10.9334C44.809 9.7403 44.809 8.26945 44.1199 7.0763Z" fill="#884DF8" />
+                        <path d="M37.4464 22.4997C38.1356 21.3066 38.1356 19.8357 37.4464 18.6426L27.7984 1.93345C27.1093 0.740311 25.8339 0.00488281 24.4556 0.00488281H17.775L34.1036 28.2854L37.4413 22.4997H37.4464Z" fill="#884DF8" />
+                        <path d="M20.7514 35.9994C22.8817 35.9994 24.6086 34.2725 24.6086 32.1423C24.6086 30.0121 22.8817 28.2852 20.7514 28.2852C18.6212 28.2852 16.8943 30.0121 16.8943 32.1423C16.8943 34.2725 18.6212 35.9994 20.7514 35.9994Z" fill="#884DF8" />
+                        <path d="M34.1073 35.9994C36.2375 35.9994 37.9644 34.2725 37.9644 32.1423C37.9644 30.0121 36.2375 28.2852 34.1073 28.2852C31.977 28.2852 30.2501 30.0121 30.2501 32.1423C30.2501 34.2725 31.977 35.9994 34.1073 35.9994Z" fill="#884DF8" />
+                        <path d="M14.4463 1.92857C13.7571 0.735428 12.4817 0 11.1086 0H4.428H0L4.45885 7.71428H8.8817L20.7565 28.2805L24.0994 22.4948C24.7885 21.3017 24.7885 19.8308 24.0994 18.6377L14.4463 1.92857Z" fill="#884DF8" />
+                    </svg>
 
-                        $profile_picture = get_user_meta($user_id, 'profile_picture', true);
+                    <span class="cart-count"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
+                </a>
 
-                        $avatar_url         = get_avatar_url($user_id);
+                <?php
+                // Check if user is logged in
+                if (is_user_logged_in()) {
+                    $user_id            = get_current_user_id();
 
-                        $current_user       = wp_get_current_user();
-                        $user_name          = $current_user->display_name;
-                        $author_archive_url = get_author_posts_url($user_id);
-                    ?>
-                        <div class="logged-in-user-wrap">
-                            <div class="thumb">
+                    $profile_picture = get_user_meta($user_id, 'profile_picture', true);
+
+                    $avatar_url         = get_avatar_url($user_id);
+
+                    $current_user       = wp_get_current_user();
+                    $user_name          = $current_user->display_name;
+                    $author_archive_url = get_author_posts_url($user_id);
+                ?>
+                    <div class="logged-in-user-wrap">
+                        <div class="thumb">
+                            <img class="avatar" src="<?php echo empty($profile_picture) ? esc_url($avatar_url) : esc_url($profile_picture); ?>" alt="User Avatar">
+                        </div>
+                        <div class="logged-in-user-info">
+                            <a class="thumb" href="<?php echo home_url('my-account'); ?>">
                                 <img class="avatar" src="<?php echo empty($profile_picture) ? esc_url($avatar_url) : esc_url($profile_picture); ?>" alt="User Avatar">
-                            </div>
-                            <div class="logged-in-user-info">
-                                <a class="thumb" href="<?php echo home_url('my-account'); ?>">
-                                    <img class="avatar" src="<?php echo empty($profile_picture) ? esc_url($avatar_url) : esc_url($profile_picture); ?>" alt="User Avatar">
-                                </a>
-                                <div class="content">
-                                    <div class="logged-in-user-name">
-                                        <a href="<?php echo home_url('my-account'); ?>">
-                                            <?php echo esc_html($user_name); ?>
-                                        </a>
-                                    </div>
-                                    <div class="user-profile">
-                                        <?php if (is_user_logged_in()) { ?>
-                                            <a href="<?php echo get_permalink(get_option('woocommerce_myaccount_page_id')); ?>"
-                                                title="<?php _e('My Account', 'woothemes'); ?>"><?php _e('My Account', 'woothemes'); ?></a>
-                                        <?php } else { ?>
-                                            <a href="<?php echo get_permalink(get_option('woocommerce_myaccount_page_id')); ?>"
-                                                title="<?php _e('Login / Register', 'woothemes'); ?>"><?php _e('Login / Register', 'woothemes'); ?></a>
-                                        <?php } ?>
-                                    </div>
-                                    <a class="log-out-btn"
-                                        href="<?php echo wp_logout_url(home_url()); ?>"><?php echo esc_html('Logout') ?></a>
+                            </a>
+                            <div class="content">
+                                <div class="logged-in-user-name">
+                                    <a href="<?php echo home_url('my-account'); ?>">
+                                        <?php echo esc_html($user_name); ?>
+                                    </a>
                                 </div>
+                                <div class="user-profile">
+                                    <?php if (is_user_logged_in()) { ?>
+                                        <a href="<?php echo get_permalink(get_option('woocommerce_myaccount_page_id')); ?>"
+                                            title="<?php _e('My Account', 'woothemes'); ?>"><?php _e('My Account', 'woothemes'); ?></a>
+                                    <?php } else { ?>
+                                        <a href="<?php echo get_permalink(get_option('woocommerce_myaccount_page_id')); ?>"
+                                            title="<?php _e('Login / Register', 'woothemes'); ?>"><?php _e('Login / Register', 'woothemes'); ?></a>
+                                    <?php } ?>
+                                </div>
+                                <a class="log-out-btn"
+                                    href="<?php echo wp_logout_url(home_url()); ?>"><?php echo esc_html('Logout') ?></a>
                             </div>
                         </div>
-                    <?php } else { ?>
-
-                        <div class="btn-wrap">
-                            <a href="<?php echo home_url('my-account'); ?>" class="boxed-btn login-btn" tabindex="0">Log in</a>
-                        </div>
-                <?php }
-                } ?>
-            </div>
+                    </div>
+                <?php } else { ?>
+                    <div class="btn-wrap">
+                        <a href="<?php echo home_url('my-account'); ?>" class="boxed-btn login-btn" tabindex="0">Log in</a>
+                    </div>
+            <?php }
+                echo '</div>';
+            } ?>
 
         </div>
     </div>
@@ -153,16 +141,6 @@ $vat_checked = (isset($_COOKIE['exclude_tax']) && $_COOKIE['exclude_tax'] == 'ye
             if (is_plugin_active('woocommerce/woocommerce.php')) {
                 require_once __DIR__ . '/category-mega-menu-mobile.php';
             }
-
-            if (wp_is_mobile()) {
-            ?>
-                <label class="tax-switch" style="display: flex; align-items: center; cursor: pointer; margin-right: 24px;">
-                    Exclude VAT
-                    <input type="checkbox" id="tax_toggle"
-                        style="margin-right: 5px;" <?php echo $vat_checked; ?>>
-                    <span class="slider round"></span>
-                </label>
-            <?php }
             ?>
         </div>
     </div>
